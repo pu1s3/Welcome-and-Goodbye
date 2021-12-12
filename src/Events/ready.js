9f3202b97b2cf24b84f564a022dd3fdf2677285f
pu1s3 <73364683+pu1s3@users.noreply.github.com>
2021-12-12 03:54:18 +0000

    Update ready.js

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -4,11 +4,4 @@ const Event = require("../Structures/Event.js");
 
 module.exports = new Event("ready", client => {
 	console.log(client.user.tag + ' has logged in.');
-	client.user.setPresence({
-		activity: {
-		  name: `People Joining :) And Leaving :(.`,
-		  type: "WATCHING"
-		},
-		status: 'dnd'
-	  })
 });
